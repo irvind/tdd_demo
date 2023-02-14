94215815193a6f3d759d6539e5ecff63bd5a7ac9
Eugene Obukhov <irvind25@gmail.com>
2023-02-14 19:12:13 +0300

    Add test_api_method_called

---
+++ tdd/external.py
@@ -0,0 +1,5 @@
+import requests
+
+
+def extract_data_from_external_api():
+    pass
--- tests/test_external.py
+++ tests/test_external.py
@@ -1,4 +1,12 @@
+from tdd.external import extract_data_from_external_api
+
+
 def test_example():
     assert True
     assert 1 + 1 == 2
 
+
+def test_api_method_called(mocker):
+    get_mock = mocker.patch('tdd.external.requests.get')
+    extract_data_from_external_api()
+    get_mock.assert_called_with('http://example.com/api/v1/user_data')
