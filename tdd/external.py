0eaa14cb466509243c5ad0fc975e52ef09c7a5f4
Eugene Obukhov <irvind25@gmail.com>
2023-02-14 19:17:36 +0300

    Add test_error_if_response_status_is_correct

--- tdd/external.py
+++ tdd/external.py
@@ -1,5 +1,9 @@
 import requests
 
 
+class ExternalApiError(Exception):
+    pass
+
+
 def extract_data_from_external_api():
     requests.get('http://example.com/api/v1/user_data')
--- tests/test_external.py
+++ tests/test_external.py
@@ -1,4 +1,6 @@
-from tdd.external import extract_data_from_external_api
+from unittest.mock import MagicMock
+import pytest
+from tdd.external import extract_data_from_external_api, ExternalApiError
 
 
 def test_example():
@@ -10,3 +12,12 @@ def test_api_method_called(mocker):
     get_mock = mocker.patch('tdd.external.requests.get')
     extract_data_from_external_api()
     get_mock.assert_called_with('http://example.com/api/v1/user_data')
+
+
+def test_error_if_response_status_is_correct(mocker):
+    mocker.patch(
+        'tdd.external.requests.get',
+        return_value=MagicMock(status_code=500)
+    )
+    with pytest.raises(ExternalApiError):
+        extract_data_from_external_api()
