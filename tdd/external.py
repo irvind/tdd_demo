7e05d51fa6b891c4a21c4341cf263562efed6056
Eugene Obukhov <irvind25@gmail.com>
2023-02-14 19:30:15 +0300

    Implement test_return_empty_dict_if_result_is_empty

--- tdd/external.py
+++ tdd/external.py
@@ -9,3 +9,7 @@ def extract_data_from_external_api():
     resp = requests.get('http://example.com/api/v1/user_data')
     if resp.status_code != 200:
         raise ExternalApiError('Response status code is invalid')
+
+    resp_json = resp.json()
+    if resp_json == []:
+        return {}
